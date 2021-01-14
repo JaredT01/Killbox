6bb6144dbf72acd37fabebe546ea8afa67424ce6
RoyalGuilds <Jared.trenholm12@gmail.com>
2021-01-14 15:00:55 -0400

    Initial commit

---
+++ Assets/Killbox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killbox : MonoBehaviour
+{
+    Vector3 spawn;
+    Quaternion rotate;
+
+
+    void Start()
+    {
+        spawn = gameObject.transform.position;
+        rotate = gameObject.transform.rotation;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Killbox")
+        {
+            Debug.Log("TRIGGERED!");
+            this.transform.position = spawn;
+            this.transform.rotation = rotate;
+            
+        }
+    }
+}
